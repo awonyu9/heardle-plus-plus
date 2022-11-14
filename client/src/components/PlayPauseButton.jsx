7d9fe5de626cb098363158e2295a3e60dfde2c9d
Alexandra Wonyu <alexandrawonyu@gmail.com>
2022-11-13 22:36:02 -0600

    add icons directory & create PlayPauseButton component

---
+++ client/src/components/PlayPauseButton.jsx
@@ -0,0 +1,8 @@
+export default function PlayPauseButton(props) {
+  return (
+    <div className="play-pause-buttons">
+      <div className="play-button"></div>
+      <div className="pause-button"></div>
+    </div>
+  )
+}
