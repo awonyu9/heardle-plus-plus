1d4b21f38c902de072e160d00a2c8e59b0312a4c
Alexandra Wonyu <alexandrawonyu@gmail.com>
2022-12-13 22:29:11 -0600

    clean up ignored files

--- client/src/components/PlayPauseButton.jsx
+++
@@ -1,8 +0,0 @@
-export default function PlayPauseButton(props) {
-  return (
-    <div className="play-pause-buttons">
-      <div className="play-button"></div>
-      <div className="pause-button"></div>
-    </div>
-  )
-}
